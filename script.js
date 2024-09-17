38364d95e40b3e62ed06a053bc259fe3742247c8
robertoschlott <163012909+robertoschlott@users.noreply.github.com>
2024-09-17 08:31:56 -0300

    salvar

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 var titulo = document.querySelector('h1');
 titulo.textContent = "Aparecida Nutricionista";
+
+var paulo = document.querySelector('#peciente1');
+var peso = paulo.querySelector('.info-peso').textContent;
+var altura = paulo.querySelector('.info-altura').textContent;
+
+var imc = peso / (altura * altura);
+
+paulo.querySelector('.info-imc').textContent = imc;
+
