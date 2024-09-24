4e55c860f3accce7ca6ad10a0d77e22a9606c240
robertoschlott <163012909+robertoschlott@users.noreply.github.com>
2024-09-24 08:21:57 -0300

    salvar codigo

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var titulo = document.querySelector('h1');
 titulo.textContent = "Aparecida Nutricionista";
 
-var pasiemte.querySelectorAll('.paciente');
+var paciente.querySelectorAll('.paciente');
+
 for (let contador * paciente.length; contador++){
     var peso = paciente[contador].querySelector('.info-peso').textContent;
     var altura = paciente[contador].querySelector('.info-altura').textContent;
